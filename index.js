48a776f3e8452fcf0172b5bd8aa8b57b07b94b99
iamsatyam77 <iamsatyam77@gmail.com>
2022-05-07 15:50:15 +0530

    feat: Added peer to peer chat

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static('public'));
+const connections = [];
+const clients = [];
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -13,10 +16,46 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
+  connections.push(socket);
+  clients.push({ socketId: socket.id });
+  console.log("Connected: %s sockets connected ", connections.length);
 
-  socket.on('init', (msg) => console.log('init message received from client', msg));
+  socket.on("addRoom", async (room) => {
+    const clients = io.sockets.adapter.rooms.get(room);
+    const numClients = typeof clients !== "undefined" ? clients.size : 0;
+    if (numClients > 1) {
+      console.log("already_full");
+    } else if (numClients === 1) {
+      socket.join(room);
+      io.in(room).emit("userJoined");
+      // socket.to(room).emit("userJoined", socket.id);
+    } else {
+      socket.join(room);
+    }
+  });
+
+  socket.on('leaveRoom', async (room) => {
+    socket.leave(room);
+  })
+
+  socket.on("sendMessage", (info) => {
+    const parsedInfo = JSON.parse(info);
+    // if (!parsedInfo.socketId) {
+    //   io.sockets.to(parsedInfo.room).emit("messageFromPeer", info);
+    // } else {
+    //   io.to(parsedInfo.socketId).emit("messageFromPeer", info);
+    // }
+    socket.to(parsedInfo.room).emit("messageFromPeer", info);
+  });
+
   socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id);
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Disconnected: %s sockets connected, ", connections.length);
+    clients.forEach((client, i) => {
+      if (client.socketId === socket.id) {
+        clients.splice(i, 1);
+      }
+    });
   });
 });
 
--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,161 @@
 const socket = io();
 
-socket.emit('init', 'hello');
+const constraints = {
+  video: true,
+  audio: true,
+};
+
+const peerConnectionConfig = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+};
+
+let localStream;
+let remoteStream;
+let peerConnection;
+
+const init = async () => {
+  try {
+    await initializeChannelAndListeners();
+
+    // const devices = await navigator.mediaDevices.enumerateDevices();
+    // const videoDevice = devices.filter((device) => device.kind === 'videoinput');
+    // console.log(`Media devices are: ${JSON.stringify(devices)}`);
+    localStream = await navigator.mediaDevices.getUserMedia(constraints);
+    document.getElementById("user-1").srcObject = localStream;
+  } catch (error) {
+    console.error("Error accessing media devices.", error);
+  }
+};
+
+const createPeerConnection = async (socketId) => {
+  peerConnection = new RTCPeerConnection(peerConnectionConfig);
+
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = remoteStream;
+  if (!localStream) {
+    localStream = await navigator.mediaDevices.getUserMedia(constraints);
+    document.getElementById("user-1").srcObject = localStream;
+  }
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = async (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+    // document.getElementById("user-2").srcObject = event.streams[0];
+  };
+
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      console.log("New ICE candidate:", event);
+      socket.emit(
+        "sendMessage",
+        JSON.stringify({
+          // socketId,
+          room: "main",
+          type: "candidate",
+          candidate: event.candidate,
+        })
+      );
+    }
+  };
+
+  peerConnection.onicecandidateerror = async (event) => {
+    console.log("ice candidate error", JSON.stringify(event));
+  };
+};
+
+const createOffer = async (socketId) => {
+  await createPeerConnection(socketId);
+
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  console.log("Offer:", offer, socket.id, socketId);
+  socket.emit(
+    "sendMessage",
+    JSON.stringify({
+      // socketId: socketId,
+      room: "main",
+      type: "offer",
+      offer,
+    })
+  );
+};
+
+const createAnswer = async (socketId, offer) => {
+  await createPeerConnection(socketId);
+
+  await peerConnection.setRemoteDescription(offer);
+
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+
+  console.log("Answer:", answer);
+  socket.emit(
+    "sendMessage",
+    JSON.stringify({
+      // socketId: socketId,
+      room: "main",
+      type: "answer",
+      answer,
+    })
+  );
+};
+
+const addAnswer = async (answer) => {
+  if (!peerConnection.currentRemoteDescription) {
+    peerConnection.setRemoteDescription(answer);
+  }
+};
+
+const handleUserJoined = async (socketId) => {
+  console.log(`User with socket Id ${socketId} has joined the room`);
+  await createOffer(socketId);
+};
+
+const handleOnMessageFromPeer = async (message) => {
+  message = JSON.parse(message);
+  console.log(message);
+
+  switch (message.type) {
+    case "offer":
+      await createAnswer(message.socketId, message.offer);
+      break;
+
+    case "candidate":
+      if (peerConnection) {
+        peerConnection.addIceCandidate(message.candidate);
+      }
+      break;
+
+    case "answer":
+      await addAnswer(message.answer);
+      break;
+
+    default:
+      break;
+  }
+};
+
+const handleUserLeft = async (event) => {
+  socket.emit("leaveRoom", "main");
+};
+
+const initializeChannelAndListeners = async () => {
+  socket.emit("addRoom", "main");
+  socket.on("userJoined", handleUserJoined);
+  socket.on("userleft", handleUserLeft);
+  socket.on("messageFromPeer", handleOnMessageFromPeer);
+};
+
+window.addEventListener("beforeunload", handleUserLeft);
+
+init();
